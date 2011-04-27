e29a65294fdbf38e226c2e1d6e7a1aa709e99a64
moxi.sos@gmail.com <moxi.sos@gmail.com@fd1f1188-bd17-c417-f66c-c669cb5d6947>
2011-04-27 01:56:40 +0000

    added display guitar function

---
+++ Code/touchscreen_map_gfxlcd/display_guitar.c
@@ -0,0 +1,32 @@
+
+//proper h files to be included
+
+//add this functions to the c file that dis
+
+void draw_guitar(void)
+{
+  small_delay(700);
+  line(1,0,7,127,7);
+  line(1,0,8,127,8);
+  line(1,0,9,127,9);
+  
+  small_delay(700);
+  line(1,0,18,127,18);
+  line(1,0,19,127,19);
+
+  small_delay(700);
+  line(1,0,28,127,28);
+  line(1,0,29,127,29);
+
+  small_delay(700);
+  line(1,0,39,127,39);
+
+  small_delay(700);
+  line(1,0,48,127,48);
+
+  small_delay(700);
+  line(1,0,57,127,57);
+  
+  small_delay(700);
+
+}
